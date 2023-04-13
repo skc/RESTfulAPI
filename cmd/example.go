c7f3e7e89f69d459c581d058b52ad1b3695faf53
Shimon K. Cohen <shimoco@gmail.com>
2023-04-13 20:10:38 +0300

    custome handler

--- cmd/example.go
+++ cmd/example.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+func Handler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+		"name":    name,
+	})
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping/:name", Handler)
 	r.Run(":5001") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
